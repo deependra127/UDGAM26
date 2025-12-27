3940d73fe9212929fad594da734848157b9de0c8
a-kakade <a.kakade@iitg.ac.in>
2025-12-27 10:07:56 +0530

    OurTeam added

---
+++ src/OurTeam/OurTeam.jsx
@@ -0,0 +1,227 @@
+import React from 'react' 
+import './OurTeam.css';
+import convenerPhoto from "../assets/ourteam/convinormal.png";
+// import transparent from "../assets/transparent.png";
+import subtract from "../assets/ourteam/Subtract.svg";
+import Rectangle14 from "../assets/Rectangle14.svg";
+import our from "../assets/ourteam/our.png";
+// import rectangleleft from "../assets/rectangle-left.svg";
+// import design from "../assets/design.svg";
+// import ls from "../assets/ls.svg";
+// import disrupt from "../assets/disrupt.svg";
+import disrupt2 from "../assets/ourteam/disrupt2.svg";
+// import expo from "../assets/expo.svg";
+import expo2 from "../assets/ourteam/expo2.svg";
+// import ifbg from "../assets/ifbg.svg";
+import if1 from "../assets/ourteam/if1.svg";
+import if2 from "../assets/ourteam/if2.svg";
+// import ina from "../assets/ina.svg";
+// import marky from "../assets/marky.svg";
+// import prb from "../assets/prb.svg";
+// import website from "../assets/website.svg";
+// import sparkle from "../assets/sparkle.svg";
+import sparkle2 from "../assets/ourteam/sparkle2.svg";
+import purple from "../assets/ourteam/purple.svg";
+import pmxpng from "../assets/ourteam/pmxpng.png";
+import ifpng from "../assets/ourteam/ifpng.png";
+// import websitebg from "../assets/websitebg.svg";
+import webpng from "../assets/ourteam/webpng.png";
+import designpng from "../assets/ourteam/designpng.png";
+import sparklepng from "../assets/ourteam/sparklepng.png";
+import lspng from "../assets/ourteam/lspng.png";
+import inapng from "../assets/ourteam/inapng.png";
+import disruptpng from "../assets/ourteam/disruptpng.png";
+import marketingpng from "../assets/ourteam/marketingpng.png";
+import prbpng from "../assets/ourteam/prbpng.png";
+import expopng from "../assets/ourteam/expopng.png";
+const OurTeam = () => {
+    return (
+            <section className="our-team">
+                  <div className='Heading'>
+                    <div style={{color: '#FFF9F0', fontSize: 116.78, fontFamily: 'Montserrat', fontWeight: '900', wordWrap: 'break-word'}}>OUR TEAM</div>
+                  </div>
+            <div className='meet-our-heads'>
+                <div className="col1">
+                    <div className='image-container1' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                    <div className='image-container2' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    <img src={subtract} className="subtract-left" />
+                    </div>
+
+                    <div className='image-container3' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                    <div className='image-container4' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+                </div>
+                
+                <div className="col2">
+                    <div className='meet' style={{backgroundImage: `url(${our})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center', borderRadius: '2.95vw'}} >
+                        <div className="meet-text">MEET</div>
+                        <img src={subtract} className="subtract-top" /> 
+                    </div>
+                    
+                    
+                    <div className='image-container5' >
+                <div className="card">
+                            <img src={convenerPhoto} alt="profile" />
+                            <div className="text-cut">
+                    <div className='Name'>TANISH AGARWAL</div>
+                                <div className='POR'>CONVENER</div>
+                                </div>
+                    </div>
+                    </div>
+                    
+                <div className='image-container6' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                <div className='image-container7' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+                
+                
+                </div>
+                
+                <div className="col3">
+                    <div className='our' style={{backgroundImage: `url(${our})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center', borderRadius: '2.95vw'}} >
+                    <div className="our-text">OUR</div>
+                    </div>
+
+                    <div className='image-container8' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                    <div className='image-container9' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                    <div className='image-container10' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                
+                </div>
+                
+                <div className="col4">
+                <div className='heads' style={{backgroundImage: `url(${our})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center', borderRadius: '2.95vw'}} >
+                    <div className="heads-text">HEADS</div>
+                    </div>
+
+                <div className='image-container11' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                <div className='image-container12' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                    <div className='POR'>CONVENER</div>
+                    </div>
+
+                <div className='image-container13' style={{backgroundImage: `url(${convenerPhoto})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                    <div className='Name'>TANISH AGARWAL</div>
+                        <div className='POR'>CONVENER</div>
+                        <img src={subtract} className="subtract-right" />
+                    </div>
+                
+            
+                </div>
+                
+                
+
+                
+
+                
+                
+                
+                
+                
+                
+                
+                
+                    
+            </div>
+            <div className='managers-heading'>
+                    <div style={{color: '#FFF9F0', fontSize: 80, fontFamily: 'Montserrat', fontWeight: '900', wordWrap: 'break-word'}}>OUR MANAGERS</div>
+                </div>
+            <div className="managers">
+                <div className="column1">
+                    <div className="pmx" style={{backgroundImage: `url(${pmxpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="pmx-text">PMX &<br />ENCODE</div>
+                    </div>
+                    <div className="if" style={{backgroundImage: `url(${ifpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="if-text">INTERNFAIR</div>
+                        <img src={if1} className='if-image1' />
+                        <img src={if2} className='if-image2' />
+                    </div>
+                    <div className="website" style={{backgroundImage: `url(${webpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="website-text">WEBSITE</div>
+                    </div>
+                </div>
+                <div className="column2">
+                    <div className="design" style={{backgroundImage: `url(${designpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="design-text">DESIGN</div>
+                        <img src={purple} className='purple-left' />
+                        <img src={purple} className='purple-top' />
+                    </div>
+                    <div className="sparkle" style={{backgroundImage: `url(${sparklepng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="sparkle-text">SPARKLE</div>
+                        <img src={sparkle2} className='sparkle-image1' />
+                        
+                    </div>
+                </div>
+                
+                <div className="column3">
+                    <div className="ls" style={{backgroundImage: `url(${lspng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="ls-text">LECTURE <br/>SERIES</div>
+                    </div>
+                    <div className="ina" style={{backgroundImage: `url(${inapng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="ina-text">INA</div>
+                    </div>
+                    <div className="website" style={{backgroundImage: `url(${webpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="website-text">WEBSITE</div>
+                    </div>
+                </div>
+
+                <div className="column4">
+                    <div className="disrupt" style={{backgroundImage: `url(${disruptpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="disrupt-text">DISRUPT</div>
+                        <img src={disrupt2} className='disrupt-image1' />
+                    </div>
+                    <div className="marky" style={{backgroundImage: `url(${marketingpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="marky-text">MARKETING</div>
+                    </div>
+                </div>
+                <div className="column5">
+                    <div className="expo" style={{backgroundImage: `url(${expopng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="expo-text">STARTUP<br />EXPO</div>
+                        <img src={expo2} className='expo-image1' />
+                    </div>
+                    <div className="prb" style={{backgroundImage: `url(${prbpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="prb-text">PR <br />BRANDING</div>
+                        <img src={purple} className='purple-rtop' />
+                        <img src={purple} className='purple-rbottom' />
+                    </div>
+                    <div className="website" style={{backgroundImage: `url(${webpng})`, backgroundSize: 'cover', noRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+                        <div className="website-text">WEBSITE</div>
+                    </div>
+                </div>
+
+            </div>
+                </section>
+        );
+}; export default OurTeam;
